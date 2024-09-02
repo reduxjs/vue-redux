e5f6007e3f8c3fc48e2bb388f2b6b6938ee6c14a
Corbin Crutchley <git@crutchcorn.dev>
2024-09-02 03:02:39 -0700

    chore: initial commit

---
+++ examples/vue/simple/src/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>Testing</p>
+</template>
---
+++ packages/vue-redux/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, mergeConfig } from 'vitest/config'
+import { tanstackViteConfig } from '@tanstack/config/vite'
+import vue from '@vitejs/plugin-vue'
+import packageJson from './package.json'
+
+const config = defineConfig({
+  plugins: [vue()],
+  test: {
+    name: packageJson.name,
+    dir: './tests',
+    watch: false,
+    environment: 'jsdom',
+    setupFiles: ['./tests/test-setup.ts'],
+    coverage: { enabled: true, provider: 'istanbul', include: ['src/**/*'] },
+    typecheck: { enabled: true },
+  },
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+  },
+})
+
+export default mergeConfig(
+  config,
+  tanstackViteConfig({
+    entry: './src/index.ts',
+    srcDir: './src',
+  }),
+)
