8f17872dfe58b846ec1902f9592e5e149db49135
Corbin Crutchley <git@crutchcorn.dev>
2024-09-04 19:46:41 -0700

    chore: update generate docs

--- scripts/generateDocs.js
+++ scripts/generateDocs.js
@@ -7,45 +7,10 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url))
 /** @type {import('@tanstack/config/typedoc').Package[]} */
 const packages = [
   {
-    name: 'form-core',
-    entryPoints: [resolve(__dirname, '../packages/form-core/src/index.ts')],
-    tsconfig: resolve(__dirname, '../packages/form-core/tsconfig.docs.json'),
-    outputDir: resolve(__dirname, '../docs/reference'),
-  },
-  {
-    name: 'angular-form',
-    entryPoints: [resolve(__dirname, '../packages/angular-form/src/index.ts')],
-    tsconfig: resolve(__dirname, '../packages/angular-form/tsconfig.docs.json'),
-    outputDir: resolve(__dirname, '../docs/framework/angular/reference'),
-    exclude: ['packages/form-core/**/*'],
-  },
-  {
-    name: 'lit-form',
-    entryPoints: [resolve(__dirname, '../packages/lit-form/src/index.ts')],
-    tsconfig: resolve(__dirname, '../packages/lit-form/tsconfig.docs.json'),
-    outputDir: resolve(__dirname, '../docs/framework/lit/reference'),
-    exclude: ['packages/form-core/**/*'],
-  },
-  {
-    name: 'react-form',
-    entryPoints: [resolve(__dirname, '../packages/react-form/src/index.ts')],
-    tsconfig: resolve(__dirname, '../packages/react-form/tsconfig.docs.json'),
-    outputDir: resolve(__dirname, '../docs/framework/react/reference'),
-    exclude: ['packages/form-core/**/*'],
-  },
-  {
-    name: 'solid-form',
-    entryPoints: [resolve(__dirname, '../packages/solid-form/src/index.tsx')],
-    tsconfig: resolve(__dirname, '../packages/solid-form/tsconfig.docs.json'),
-    outputDir: resolve(__dirname, '../docs/framework/solid/reference'),
-    exclude: ['packages/form-core/**/*'],
-  },
-  {
-    name: 'vue-form',
-    entryPoints: [resolve(__dirname, '../packages/vue-form/src/index.ts')],
-    tsconfig: resolve(__dirname, '../packages/vue-form/tsconfig.docs.json'),
-    outputDir: resolve(__dirname, '../docs/framework/vue/reference'),
-    exclude: ['packages/form-core/**/*'],
+    name: 'vue-redux',
+    entryPoints: [resolve(__dirname, '../packages/vue-redux/src/index.ts')],
+    tsconfig: resolve(__dirname, '../packages/vue-redux/tsconfig.docs.json'),
+    outputDir: resolve(__dirname, '../docs/reference')
   },
 ]
 
