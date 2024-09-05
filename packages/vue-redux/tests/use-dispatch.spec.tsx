968fe5151176a46efd4200220350ef09c59de619
Corbin Crutchley <git@crutchcorn.dev>
2024-09-05 06:51:51 -0700

    test: add test for useDispatch's context passing

---
+++ packages/vue-redux/tests/use-dispatch.spec.tsx
@@ -0,0 +1,61 @@
+import {describe, it, expect} from "vitest";
+import {defineComponent, h, InjectionKey} from 'vue'
+import {
+  createDispatchComposition,
+  provideStore as provideMock,
+  useDispatch, VueReduxContextValue,
+} from '../src'
+import { createStore } from 'redux'
+import {render} from "@testing-library/vue";
+
+const store = createStore((c: number = 1): number => c + 1)
+const store2 = createStore((c: number = 1): number => c + 2)
+
+describe('Vue', () => {
+  describe('compositions', () => {
+    describe('useDispatch', () => {
+      it("returns the store's dispatch function", () => {
+        const Comp = defineComponent(() => {
+          const dispatch = useDispatch();
+          expect(dispatch).toBe(store.dispatch)
+
+          return () => null;
+        })
+
+        const App = defineComponent(() => {
+          provideMock({store});
+          return () => <Comp/>
+        })
+
+        render(<App/>)
+      })
+    })
+    describe('createDispatchComposition', () => {
+      it("returns the correct store's dispatch function", () => {
+        const nestedContext = Symbol.for("mock-redux-store") as InjectionKey<VueReduxContextValue | null>
+        const useCustomDispatch = createDispatchComposition(nestedContext)
+
+        const CheckDispatch = defineComponent(() => {
+          const dispatch = useDispatch();
+          const customDispatch = useCustomDispatch();
+          expect(dispatch).toBe(store.dispatch)
+          expect(customDispatch).toBe(store2.dispatch)
+
+          return () => null;
+        })
+
+        const InnerApp = defineComponent(() => {
+          provideMock({store: store2, context: nestedContext});
+          return () => <CheckDispatch/>
+        })
+
+        const App = defineComponent(() => {
+          provideMock({store});
+          return () => <InnerApp/>
+        })
+
+        render(<App/>)
+      })
+    })
+  })
+})
